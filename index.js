a43c076882937af1f7a7d742de8cfdbcaa9117ba
SivaranjithaSivaraj <sivaranjitha.sivaraj@codingmart.com>
2019-12-23 15:04:51 +0530

    changed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var x= document.getElementById("ip");
 var y=0;
 document.getElementById("next").addEventListener("click",function(){
-    y=-(parseInt(x.value)+220);
-    if(y*-1<=1000){
+    y=-(parseInt(x.value)+640);
+    if(y*-1<=2000){
         console.log(y)
         document.getElementById("scroll1").style.transform="translateX("+y+"px)";
         x.value=-y;
@@ -11,7 +11,7 @@ document.getElementById("next").addEventListener("click",function(){
 var a= document.getElementById("ip");
 var b=0;
 document.getElementById("prev").addEventListener("click",function(){
-    b=-(parseInt(a.value)-220);
+    b=-(parseInt(a.value)-640);
     if(b<=0){
         console.log(b)
         document.getElementById("scroll1").style.transform="translateX("+b+"px)";
